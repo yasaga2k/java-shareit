3741c82cec4ac728ae8499f54ff1685dcfc34920
Egor Zauzolkin <qkkkkkkkkkkkk@yandex.ru>
2025-11-12 16:56:53 +0300

    ссылки

--- server/src/main/java/ru/practicum/shareit/exceptions/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
